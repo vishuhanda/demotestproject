ae9d972ae1bdfc81cb3c0bad75125a159c1b19d6
vishuhanda <itsvishuhanda@gmail.com>
2019-04-13 18:18:45 +0530

    second commit

---
+++ src/test/java/demotestproject/LoginTest.java
@@ -0,0 +1,16 @@
+package demotestproject;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	
+	@Test
+	public void doLogin() {
+		assertEquals(true, true);
+	}
+	
+	
+}
